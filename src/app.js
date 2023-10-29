29d587feb31766124e6b81defa90c7c1a64a70a7
Regina Askerova <saparova.regina@yandex.ru>
2023-10-29 19:40:36 +0300

    first_hw

--- src/app.js
+++ src/app.js
@@ -3,34 +3,47 @@ const { URL } = require("url");
 const getUsers = require("./modules/users");
 const server = http.createServer((request, response) => {
   const url = new URL(request.url, "http://127.0.0.1");
-  console.log(url);
-  console.log(url.searchParams);
+  console.log(url.searchParams.keys());
+
   const nameFromURL = url.searchParams.get("hello");
+
   if (request.url === `/?hello=${nameFromURL}`) {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write(`Hello, ${url.searchParams.get("hello")}`);
-    response.end();
-    return;
-  } else if (request.url === "/hello") {
-    response.status = 400;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write("Enter a name");
-    response.end();
-    return;
+    if (nameFromURL !== "") {
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      response.setHeader("Content-Type", "application/json");
+      response.write(`Hello, ${url.searchParams.get("hello")}`);
+      response.end();
+      return;
+    } else {
+      response.statusCode = 400;
+      response.statusMessage = "Bad request";
+      response.setHeader("Content-Type", "application/json");
+      response.write("Enter a name");
+      response.end();
+      return;
+    }
   } else if (request.url === "/users") {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+    response.setHeader("Content-Type", "application/json");
     response.write(getUsers());
     response.end();
     return;
+  } else if (
+    url.searchParams.keys().length !== 0 &&
+    !url.searchParams.has("hello")
+  ) {
+    response.statusCode = 500;
+    response.statusMessage = "Error";
+    response.setHeader("Content-Type", "text/plain");
+    response.write(" ");
+    response.end();
+    return;
   }
-  response.status = 200;
+  response.statusCode = 200;
   response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
+  response.setHeader("Content-Type", "text/plain");
   response.write("Hello, world!");
   response.end();
   return;
@@ -38,26 +51,3 @@ const server = http.createServer((request, response) => {
 server.listen(3000, () => {
   console.log("Сервер запущен по адресу http://127.0.0.1:3000");
 });
-
-/*const http = require("http");
-const getUsers = require("./modules/users");
-
-const server = http.createServer((request, response) => {
-  if (request.url === "/users") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write(getUsers());
-    response.end();
-    return;
-  }
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
-  response.write("Hello, world!");
-  response.end();
-});
-
-server.listen(3000, () => {
-  console.log("Сервер запущен по адресу http://127.0.0.1:3000");
-});*/
